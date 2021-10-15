42d8cb0469dc9cd4b6aa9205ad29ac99921833d3
Carlos Pinochet <crpinochet@gmail.com>
2021-10-15 12:28:02 -0300

    picture

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src="https://photos.google.com/share/AF1QipPjazjV2ieZK0vkrDJIUmT_n7HRer5NbbOMj0k_l5OBpNObUp00_bRx59jhyKUi2w/photo/AF1QipPg4Yg6ZwQX6w-XFmGRYb2tk6udx2hDn8BBtCyu?key=bDR5UTl3RmVheVpjUXd3bUVaVzZGOWlqbUd3U25n" /> 
+          <img src="https://scontent.fscl6-1.fna.fbcdn.net/v/t1.6435-9/61214228_10218301286061644_4596777611417354240_n.jpg?_nc_cat=106&amp;ccb=1-5&amp;_nc_sid=730e14&amp;_nc_eui2=AeEdnVzWgxk_nSP1uX3ybYNfy8nO35gtrbnLyc7fmC2tuTfGBug7g0p5AvzhwOvXaK8&amp;_nc_ohc=RaTJehP4a6QAX_Bgfx2&amp;_nc_ht=scontent.fscl6-1.fna&amp;oh=29de6df9ce7ddfedc0311b2c13b486d0&amp;oe=61908536" width="400" height="400">
           <h3>Yandi, la gata.</h3>
           <img src={logo} className="App-logo" alt="logo" />
           <a
