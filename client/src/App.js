0e5e035ff8ef355e6c523665a1b7f32cfac08652
Carlos Pinochet <crpinochet@gmail.com>
2021-10-18 12:10:27 -0300

    adjust image size

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src="https://cpinochet.github.io/images/yandi.jpg" />
+          <img src="https://cpinochet.github.io/images/yandi.jpg" width="400" height="400"/>
           <h3>Yandi, la gata.</h3>
           <img src={logo} className="App-logo" alt="logo" />
           <a
